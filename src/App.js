411d5b5483f25784f18705a094cafac5734417a7
mustafa7egazi <mustafareda3210@gmail.com>
2021-12-07 20:21:52 +0200

    Created cv page with React

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import SideBar from "./components/SideBar";
+
+function App() {
+  return (
+    <div className='container'>
+      <SideBar />
+      <Body />
+      <br />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Body.jsx
@@ -0,0 +1,157 @@
+export default function Body() {
+  return (
+    <div className="rightParent">
+    <div className="right">
+      <div className="upper">
+        <h3 style={{ float: "left", paddingLeft: "5px", marginTop: "5px" }}>
+          <i class="fas fa-briefcase"></i>
+          Work Experience
+        </h3>
+        <br />
+        <h4
+          style={{ marginTop: "20px", marginLeft: "5px", paddingLeft: "35px" }}
+        >
+          Moblie Developer
+        </h4>
+        <br />
+
+        <h4 style={{ float: "left", paddingLeft: "5px", marginTop: "5px" }}>
+          <i class="fas fa-calendar-week"></i>
+           &nbsp; Jan2015 - current
+        </h4>
+        <br />
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisi
+          metus, commodo eu maximus ac, pretium et ante. Vestibulum elementum
+          aliquam enim, nec fermentum nunc sagittis ac. Sed erat tortor, varius
+          sed fringilla vitae, vulputate vel elit. Quisque dapibus ex vitae
+          neque ullamcorper posuere. Etiam urna nisi, varius vel neque
+          ultricies, accumsan posuere augue. Duis hendrerit, justo varius tempus
+          pulvinar, massa nisi tincidunt magna, vel congue nisl lacus non lorem.
+          In feugiat velit a ipsum posuere, nec fringilla mi luctus. Mauris in
+          libero placerat, suscipit dui eu, egestas elit. Nulla eget ultricies
+          eros. Pellentesque habitant morbi tristique senectus et netus et
+          malesuada fames ac turpis egestas. Sed vestibulum massa at enim semper
+          egestas.
+        </p>
+        <br />
+        <br />
+        <hr />
+        <br />
+        <h4
+          style={{ marginTop: "20px", marginLeft: "5px", paddingLeft: "35px" }}
+        >
+          FrontEnd Developer
+        </h4>
+        <br />
+        <h4 style={{ float: "left", paddingLeft: "5px", marginTop: "5px" }}>
+          <i class="fas fa-calendar-week"></i>
+          &nbsp; May2013 - current{" "}
+        </h4>
+        <br />
+        <br />
+        <p>
+          Donec faucibus pellentesque tortor, eget placerat tortor fermentum at.
+          Vestibulum sed sagittis enim, tincidunt dignissim dui. Sed egestas
+          laoreet velit in iaculis. Phasellus in tellus in risus aliquet
+          malesuada. Aliquam faucibus pretium nunc, at sollicitudin ante semper
+          vitae. Nam maximus augue purus, eu pellentesque turpis mattis quis.
+          Orci varius natoque penatibus et magnis dis parturient montes,
+          nascetur ridiculus mus. Nunc non elementum sem, placerat dapibus ex.
+          Vivamus orci sem, facilisis sit amet massa id, tincidunt rhoncus ante.
+          Praesent in arcu ex. Duis a aliquam elit. Suspendisse vitae nunc
+          lacinia turpis ultricies commodo non quis odio. Vivamus hendrerit
+          semper sem, ut volutpat diam accumsan sit amet.
+        </p>
+        <hr />
+        <br />
+        <h4
+          style={{ marginTop: "20px", marginLeft: "5px", paddingLeft: "35px" }}
+        >
+          C++ Developer
+        </h4>
+        <br />
+
+        <h4 style={{ float: "left", paddingLeft: "5px", marginTop: "5px" }}>       
+         <i class="fas fa-calendar-week"></i>
+         &nbsp; Feb2011 - current
+        </h4>
+        <br />
+        <br />
+        <p>
+          Curabitur eu turpis neque. Donec hendrerit consequat mauris ac
+          pharetra. Fusce ac bibendum tortor. Vivamus dui nulla, maximus in
+          congue a, mollis non sapien. Integer non velit odio. Fusce pulvinar,
+          metus nec scelerisque dictum, quam enim elementum est, in viverra
+          ipsum mauris ac ex. Mauris vehicula, diam non condimentum ullamcorper,
+          leo nibh mollis enim, sed pretium dolor urna ac ipsum. Etiam cursus
+          erat tortor, mollis tincidunt ante tincidunt ac. Aliquam faucibus
+          lacinia nisi at ultrices. Quisque ornare iaculis elementum.
+          Pellentesque accumsan tincidunt neque ac vulputate.
+        </p>
+        <br />
+        <br />
+        <br />
+      </div>
+
+      <div className="lower">
+        <h4 style={{ float: "left", paddingLeft: "5px", marginTop: "5px" }}>
+          <i class="fas fa-calendar-week"></i>
+          &nbsp; Education
+        </h4>
+        <br />
+        <h4 style={{ float: "left", paddingLeft: "5px", marginTop: "5px" }}>
+          <i class="fas fa-calendar-week"></i>
+          &nbsp; Faculty of computers and information - Menofia university
+        </h4>
+        <br />
+        <h4 style={{ float: "left", paddingLeft: "5px", marginTop: "5px" }}>
+          <i class="fas fa-calendar-week"></i>
+          2020- current
+        </h4>
+        <br /> <br />
+        <br />
+        <br />
+        <hr /> <br />
+        <h5
+          style={{ marginTop: "20px", marginLeft: "5px", paddingLeft: "35px" }}
+        >
+          Amin El-kholy secondary school
+        </h5>
+        <br />
+        
+        <h5 style={{ float: "left", paddingLeft: "5px", marginTop: "5px" }}>
+        <i className="fas fa-calendar-week"></i>
+        &nbsp; 2019
+        </h5>
+        <br />
+        <br />
+        <br />
+     
+      </div>
+    </div>
+     <footer className="footer">
+     <p>
+       <strong>Find me on social media</strong>
+     </p>
+     &nbsp;
+     <p>
+       <i className="fab fa-facebook-square"></i>
+       &nbsp;
+       <i className="fab fa-instagram"></i>
+       &nbsp;
+       <i className="fab fa-twitter"></i>
+       &nbsp;
+       <i className="fab fa-linkedin"></i>
+     </p>
+     &nbsp;
+     <p>
+       <strong>
+         <i>Powered by Mustafa R. Hegazy2021</i>
+       </strong>
+     </p>
+   </footer>
+   </div>
+  );
+}
